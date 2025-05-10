43d2367464c38dcff78a7a5550d22c709ef3f8cb
Deric Jojo <73828512+JackitudilinksG@users.noreply.github.com>
2025-05-10 16:06:38 +0530

    proxy server

---
+++ Problem1/Server/proxy.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "http://20.244.56.144",
+    changeOrigin: true,
+    pathRewrite: { "^/api": "" },
+  })
+);
+
+app.listen(5000, () => {
+  console.log("Proxy server running on http://localhost:5000");
+});
